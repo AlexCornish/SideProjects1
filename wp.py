c352c1126207cc4c7248acbea66a9ee337271fd8
AlexCornish <64739094+AlexCornish@users.noreply.github.com>
2020-07-14 15:07:58 -0400

    Update wp.py

--- wp.py
+++ wp.py
@@ -1,13 +1,14 @@
 import BLS_Request
 import os
 import pyarrow.parquet as pq
+import time 
 import pandas as pd
 import csv
 path = str(os.path.dirname(os.path.realpath(__file__)))
 quartersArr = ["M01M02M03","M04M05M06","M07M08M09","M10M11M12"]
 
 class dataRow:
-    def __init__(self,seriesID, year, period, value, percentageChange,surveyAbbr, group_code, group_name, item_code, item_name, seasonal, timePeriod):
+    def __init__(self,seriesID, year, period, value, percentageChange,surveyAbbr, group_code, group_name, item_code, item_name, seasonal, timePeriod, yearOverYear):
         self.seriesID = seriesID # The full code that comes as default in both of the Current files. Usually looks like PCU1133--1133
         self.year = year # The year of the report.
         self.period = period # The month the report came out, usually in MXX format, with XX being the number of the month.
@@ -20,15 +21,9 @@ class dataRow:
         self.item_code = item_code # The next 6 digits following the industry_group_code, represents the product or item code for the item.
         self.item_name = item_name # Name of the product/item that corresponds to the product_item_code.
         self.timePeriod = timePeriod # A formatted publication date, in the format of YYYY-MM-01
+        self.yearOverYear = yearOverYear
     def __str__(self):
-        return "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (self.seriesID,self.year,self.period,self.value,self.surveyAbbr,self.seasonal,self.group_code,self.group_name,self.item_code, self.item_name, self.percentageChange, self.timePeriod)
-
-class labelStorage:
-    def __init__(self, item_name, item_Dict):
-        self.item_name = item_name
-        self.item_Dict = item_Dict
-    def __str__(self):
-        return "Test: Item Name:%s Dict:%s" % (self.item_name,self.item_Dict)
+        return "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (self.seriesID,self.year,self.period,self.value,self.surveyAbbr,self.seasonal,self.group_code,self.group_name,self.item_code, self.item_name, self.percentageChange, self.timePeriod,self.yearOverYear)
 
 class quarters:
     def __init__(self, q1, q2, q3, q4):
@@ -47,10 +42,7 @@ def readParquet(fileName):
 
 def writeToCSV(fileName,data):
     tempName = fileName[:-8] + ".csv"
-    with open(tempName,'w',newline='') as newFile:
-        wr = csv.writer(newFile,delimiter=',')
-        wr.writerows(formatString(str(x)).split(',') for x in data)
-        newFile.close()
+    data.to_csv(tempName,index=False)
 
 def formatTimePeriod(year,monthPeriod):
     # This should be formatted yyyy-mm-01
@@ -59,14 +51,36 @@ def formatTimePeriod(year,monthPeriod):
 def specialRounding(currentNum, previousNum):
     return round((currentNum-previousNum),1)
 
+def yearOverYearCalculation(dataFrame,dropM13):
+    newDF = []
+    labelDict = []
+    dfList = dataFrame.values.tolist()
+    for j in range(0,len(dfList)):
+        newRowQrt = dataRow(dfList[j][0],dfList[j][1],dfList[j][2],dfList[j][3],"","","","","","","","","")
+        newDF.append(newRowQrt)
+    print(len(newDF))
+    for i in range(0,len(newDF)):
+        if newDF[i].seriesID not in labelDict:
+            labelDict.append(newDF[i].seriesID)
+            newDF[i].yearOverYear = "-"
+            if dropM13 == 1:
+                for k in range(1,12):
+                    print(str(newDF[int(i+k)]))
+                    newDF[int(i+k)].yearOverYear = "-"
+            else:
+                for k in range(1,13):
+                    print(str(newDF[int(i+k)]))
+                    newDF[int(i+k)].yearOverYear = "-"
+    for l in newDF:
+        print(str(l))
+
 def quarteriseDataFrame(dataFrame):
     newDF = []
     dfList = dataFrame.values.tolist()
     iterList = iter(dfList)
-    next(iterList)
     quarterDict = {}
     for j in iterList:
-        newRowQrt = dataRow(j[0],j[1],j[2],j[3],"","","","","","","","")
+        newRowQrt = dataRow(j[0],j[1],j[2],j[3],"","","","","","","","","")
         if newRowQrt.seriesID not in quarterDict:
             quarterDict[newRowQrt.seriesID] = {}
         if newRowQrt.year not in quarterDict[newRowQrt.seriesID]:
@@ -83,38 +97,42 @@ def quarteriseDataFrame(dataFrame):
                     quarterDict[newRowQrt.seriesID][newRowQrt.year].q4.append(float(newRowQrt.value))
     for x in quarterDict:
         for k in quarterDict[x]:
-            newDF.append([x,k,"Q1",arrayAvg(quarterDict[x][k].q1),"","","","","","","",""])
-            newDF.append([x,k,"Q2",arrayAvg(quarterDict[x][k].q2),"","","","","","","",""])
-            newDF.append([x,k,"Q3",arrayAvg(quarterDict[x][k].q3),"","","","","","","",""])
-            newDF.append([x,k,"Q4",arrayAvg(quarterDict[x][k].q4),"","","","","","","",""])
-    return newDF
+            newDF.append([x,k,"Q1",arrayAvg(quarterDict[x][k].q1)])
+            newDF.append([x,k,"Q2",arrayAvg(quarterDict[x][k].q2)])
+            newDF.append([x,k,"Q3",arrayAvg(quarterDict[x][k].q3)])
+            newDF.append([x,k,"Q4",arrayAvg(quarterDict[x][k].q4)])
+    newDataFrame = pd.DataFrame(newDF, columns=["series_id","year","quarter","value"])
+    newDataFrame = newDataFrame[newDataFrame["value"]!="X"]
+    return newDataFrame
 
 def arrayAvg(arr):
     if len(arr) == 0:
-        return "-"
+        return "X"
     return round(sum(arr)/len(arr),1)
 
 def periodOverPeriodCalculation(dataFrame):
-    print("period over period ")
+    dfList = dataFrame.values.tolist()
+    percentageColumn = []
     labelDict = []
-    for i in range(0,len(dataFrame)):
-        if dataFrame[i].seriesID not in labelDict:
-            labelDict.append(dataFrame[i].seriesID)
-            dataFrame[i].percentageChange = "-"
-        elif dataFrame[i].value == "-" or dataFrame[i-1].value == "-":
-            dataFrame[i].percentageChange = "-"
+    for row in range(0,len(dfList)):
+        if dfList[row][0] not in labelDict:
+            labelDict.append(dfList[row][0])
+            percentageColumn.append("X")
+        elif dfList[row][3] == "X" or dfList[row-1][3] == "X":
+            percentageColumn.append("X")
         else:
-            dataFrame[i].percentageChange = specialRounding(float(dataFrame[i].value),float(dataFrame[i-1].value))
+            percentageColumn.append(specialRounding(float(dfList[row][3]),float(dfList[row-1][3])))
+    dataFrame.insert((dataFrame.columns.get_loc("value")+1),"percent_change",percentageColumn,True)
     return dataFrame
 
+# Have a look over this. 
 def yearifyDataFrame(dataFrame):
     newDF = []
     dfList = dataFrame.values.tolist()
     iterList = iter(dfList)
-    next(iterList)
     yearDict = {}
     for j in iterList:
-        newRowQrt = dataRow(j[0],j[1],j[2],j[3],"","","","","","","","")
+        newRowQrt = dataRow(j[0],j[1],j[2],j[3],"","","","","","","","","")
         if newRowQrt.seriesID not in yearDict:
             yearDict[newRowQrt.seriesID] = {}
         if newRowQrt.year not in yearDict[newRowQrt.seriesID]:
@@ -122,112 +140,84 @@ def yearifyDataFrame(dataFrame):
         yearDict[newRowQrt.seriesID][newRowQrt.year].append(float(newRowQrt.value))
     for x in yearDict:
         for k in yearDict[x]:
-            newDF.append([x,k,"",arrayAvg(yearDict[x][k]),"","","","","","","",""])
-    return newDF
+            newDF.append([x,k,"",arrayAvg(yearDict[x][k])])
+    return pd.DataFrame(newDF, columns=["series_id","year","year","yearly average"])
 
 def createCustomFormattedDataFrame(dataFrame):
-    columnTitlesSet = False
-    newDataFrame = []
-    outputFrame = []
-    dfList = dataFrame.values.tolist()
-    titleRow = dataRow("seriesID","year","period","value","","","","","","","","")
-
     print("For each of these options type 0 for yes or 1 for no:")
     avgOverQrt = int(input("Would you like the values averaged over quarters?: "))
     if avgOverQrt == 0:
         avgOverYear = int(input("Would you like the values averaged over the years?: "))
         if avgOverYear == 1:
-            dfList = yearifyDataFrame(dataFrame)
-            titleRow.value = "average year value"
+            dataFrame = yearifyDataFrame(dataFrame)
+        else:
+            timeFormat = int(input("Would you like the dates converted to yyyy-mm-01 format?: "))
+            m13Drop = int(input("Would you like to drop all M13 periods?: "))
     if avgOverQrt == 1:
-        dfList = quarteriseDataFrame(dataFrame)
-        titleRow.period = "quarter"
-        titleRow.value = "quarterly average value"
-    if avgOverYear == 0 and avgOverQrt == 0:
-        timeFormat = int(input("Would you like the dates converted to yyyy-mm-01 format?: "))
-        m13Drop = int(input("Would you like to drop all M13 periods?: "))
-    
-        
-
+        dataFrame = quarteriseDataFrame(dataFrame)
     percentageChg = int(input("Would you like to add the percentage change between periods?: "))
     labelAdd = int(input("Would you like to add labels for each level?: "))
-    indCom = {}
-    #___________________________________Label creation______________________________________
-    if labelAdd == 1:
-        print("BLS REQUEST LABELS")
-        BLS_Request.compareLatestOnlineVersionWithLatestDownloadedVersion("wpLRef","labels")
-        newPath = path + '\\RawData\\' + BLS_Request.getLatestVersionFileName("wpLRef",BLS_Request.getAllFilesInDirectory("wpLRef"))
-        newDataFrame = readParquet(newPath)
-        newDfList = newDataFrame.values.tolist()
-        for row in newDfList:
-            if row[1] == "-":
-                indCom[row[0]] = labelStorage(row[2],{})
-            elif row[0] not in indCom.keys():
-                indCom[row[0]] = labelStorage(row[2],{})
-                indCom.get(row[0]).item_Dict[row[1]] = row[2]
-            else:
-                if row[0] not in indCom:
-                    indCom[row[0]] = labelStorage(row[2],{})
-                else:
-                    indCom.get(row[0]).item_Dict[row[1]] = row[2]
-    
-    for k in dfList:
-        print(str(k))
-    print(type(dfList))
     codeSplit = int(input("Would you like to split all the id codes?: "))
     seasonColumn = int(input("Would you like to add a column for seasonal codes?: "))
-    iterList = iter(dfList)
-    print(type(dfList))
-    next(iterList)
-    for i in iterList: 
-        newRow = dataRow(i[0],i[1],i[2],i[3],"","","","","","","","")
-        if labelAdd == 1:
-            for i in indCom.keys():
-                if i in newRow.seriesID:
-                    newRow.group_name = indCom.get(i).item_name
-                    for j in indCom.get(i).item_Dict.keys():
-                        if j in newRow.seriesID:
-                            newRow.item_name = indCom.get(i).item_Dict[j]
-            if columnTitlesSet == False:
-                titleRow.group_name = "group_name"
-                titleRow.item_name = "item_name"
-        if codeSplit == 1:
-            newRow.surveyAbbr = newRow.seriesID[:2]
-            newRow.group_code = newRow.seriesID[3:5]
-            newRow.item_code = newRow.seriesID[5:]
-            if columnTitlesSet == False:
-                titleRow.surveyAbbr = "surveyAbbr"
-                titleRow.group_code = "group_code"
-                titleRow.item_code = "item_code"
-            if seasonColumn == 1:
-                newRow.seasonal = newRow.seriesID[2:3]
-            if columnTitlesSet == False:
-                titleRow.seasonal = "seasonal"
-        if avgOverQrt == 0 and avgOverYear == 0:
-            if timeFormat == 1:
-                newRow.timePeriod = formatTimePeriod(newRow.year,newRow.period)
-                if columnTitlesSet == False:
-                    titleRow.timePeriod = "timePeriod"
-            if m13Drop == 1:
-                if newRow.period != "M13":
-                    if columnTitlesSet == False:
-                        outputFrame.append(titleRow)
-                        columnTitlesSet = True
-                    outputFrame.append(newRow)
-            else:
-                if columnTitlesSet == False:
-                        outputFrame.append(titleRow)
-                        columnTitlesSet = True
-                outputFrame.append(newRow)
-        else:
-            if columnTitlesSet == False:
-                outputFrame.append(titleRow)
-                columnTitlesSet = True
-            outputFrame.append(newRow)
+    seasonal = []
+    groupCode = []
+    itemCode = []
     if percentageChg == 1:
-        outputFrame = periodOverPeriodCalculation(outputFrame)
-        titleRow.percentageChange = "Percentage Change"
-    return outputFrame
+        dataFrame = periodOverPeriodCalculation(dataFrame)
+    if labelAdd == 1:
+        BLS_Request.compareLatestOnlineVersionWithLatestDownloadedVersion("wpGrp","groupLabels")
+        BLS_Request.compareLatestOnlineVersionWithLatestDownloadedVersion("wpLRef","labels")
+        newPath = path + '\\RawData\\' + BLS_Request.getLatestVersionFileName("wpLRef",BLS_Request.getAllFilesInDirectory("wpLRef"))
+        newGroupPath = path + '\\RawData\\' + BLS_Request.getLatestVersionFileName("wpGrp",BLS_Request.getAllFilesInDirectory("wpGrp"))
+        newGroupFrame = changeRowHeaders(readParquet(newGroupPath)).drop([0])
+        newDataFrame = changeRowHeaders(readParquet(newPath)).drop([0])
+        mergeLeft = pd.merge(left=newGroupFrame,right=newDataFrame,how='left',left_on='group_code',right_on='group_code')
+        for row in dataFrame.index:
+            columnRow = dataFrame["series_id"][row]
+            seasonal.append(columnRow[2:3])
+            if columnRow[3:5] in newGroupFrame["group_code"].tolist():
+                groupCode.append(columnRow[3:5])
+                itemCode.append(columnRow[5:])
+            elif columnRow[3:6] in newGroupFrame["group_code"].tolist():
+                groupCode.append(columnRow[3:6])
+                itemCode.append(columnRow[6:])
+        if seasonColumn == 1:
+            dataFrame.insert((dataFrame.columns.get_loc("value")+percentageChg+1),"seasonal", seasonal, True)
+        dataFrame.insert((dataFrame.columns.get_loc("value")+percentageChg+seasonColumn+1),"group_code",groupCode,True)
+        dataFrame.insert((dataFrame.columns.get_loc("value")+percentageChg+seasonColumn+2),"item_code",itemCode,True)
+        dataFrame = pd.merge(left=dataFrame,right=mergeLeft,how='left',left_on=['group_code','item_code'],right_on=['group_code','item_code'])
+        listOfHeaders = list(dataFrame.columns)
+        print(listOfHeaders)
+        listOfHeaders[listOfHeaders.index("group_name")] = "item_code"
+        listOfHeaders[listOfHeaders.index("item_code")] = "group_name"
+        print(listOfHeaders)
+        dataFrame = dataFrame.reindex(columns=listOfHeaders)
+        print(dataFrame)
+    elif codeSplit == 1:
+        for row in dataFrame.index:
+            columnRow = dataFrame["series_id"][row]
+            seasonal.append(columnRow[2:3])
+            if columnRow[3:5] in newGroupFrame["group_code"].tolist():
+                groupCode.append(columnRow[3:5])
+                itemCode.append(columnRow[5:])
+            elif columnRow[3:6] in newGroupFrame["group_code"].tolist():
+                groupCode.append(columnRow[3:6])
+                itemCode.append(columnRow[6:])
+        if seasonColumn == 1:
+            dataFrame.insert(1,"seasonal", seasonal, True)
+        dataFrame.insert(2,"group_code",groupCode,True)
+        dataFrame.insert(3,"item_code",itemCode,True)
+
+    if avgOverQrt == 0 and avgOverYear == 0:
+        if m13Drop == 1:
+            dataFrame = dataFrame[dataFrame.period != "M13"]
+        if timeFormat == 1:
+            formattedTime = []
+            for row in dataFrame.index:
+                formattedTime.append(formatTimePeriod(dataFrame["year"][row],dataFrame["period"][row]))
+            dataFrame.insert(1,"formatted_time",formattedTime,True)
+            dataFrame = dataFrame.drop(['year','period'],axis=1)
+    return dataFrame
 
 def formatString(stringToChange):
     while ",," in stringToChange:
@@ -245,14 +235,34 @@ def changeRowHeaders(dataFrame):
 def formatPeriod(per):
     return per[1:]
 
+def experimentalQuarterisation():
+    qrtDF = pd.DataFrame(columns=['series_id','year','quarter_title','quarterly_avg'])
+    temp = dataFrame.groupby(["series_id","year"])
+    print(temp.size())
+    quarterLabel = ["Q1","Q2","Q3","Q4"]
+    t0 = time.time()
+    for key, item in temp:
+        quarter = [[],[],[],[]]
+        dfList = item.values.tolist()
+        print(dfList)
+        for row in dfList:
+            if row[2] in quartersArr[0]:
+                quarter[0].append(float(row[3]))
+            elif row[2] in quartersArr[1]:
+                quarter[1].append(float(row[3]))
+            elif row[2] in quartersArr[2]:
+                quarter[2].append(float(row[3]))
+            elif row[2] in quartersArr[3]:
+                quarter[3].append(float(row[3]))
+        for q in range(0,len(quarter)):
+            qrtDF = qrtDF.append({'series_id':dfList[0][0], 'year':dfList[0][1], 'quarter_title':quarterLabel[q],'quarterly_avg':arrayAvg(quarter[q])},ignore_index=True) 
+    t1 = time.time()
+    print(qrtDF.size)
+    print(t1-t0)
+
 def wpProcessing():
     BLS_Request.compareLatestOnlineVersionWithLatestDownloadedVersion("wpCur","Current")
     newPath = path + '\\RawData\\' + BLS_Request.getLatestVersionFileName("wpCur",BLS_Request.getAllFilesInDirectory("wpCur"))
-    #dataFrame = changeRowHeaders(readParquet(newPath)).drop([0])
-    dataFrame = readParquet(newPath)
-    #dataFrame['period'] = dataFrame['period'].apply(formatPeriod)
-    data = createCustomFormattedDataFrame(dataFrame) 
-    writeToCSV(newPath,data)
+    writeToCSV(newPath,createCustomFormattedDataFrame(changeRowHeaders(readParquet(newPath)).drop([0])))
 
 wpProcessing()
-print("Processing complete")
