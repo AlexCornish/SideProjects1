6d6470ec51a482126e6288114ad2c6b0fc3d9530
AlexCornish <64739094+AlexCornish@users.noreply.github.com>
2020-07-21 15:37:20 -0400

    Update wpAndPc.py

--- wpAndPc.py
+++ wpAndPc.py
@@ -6,6 +6,8 @@ print("Choose an option:")
 print("1: pc        (Industry)")
 print("2: wp        (Commodity)")
 wpORpc = str(input("Type either pc or wp: "))
+while wpORpc != "wp" and wpORpc != "pc":
+    wpORpc = str(input("Type either pc or wp: "))
 if wpORpc == "wp":
     wp.wpProcessing()
 elif wpORpc == "pc":
